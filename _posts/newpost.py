504f1b61a2c5a3aa5aa085f291eb914e1e9952af
Allan McInnes <allan.mcinnes@gmail.com>
2015-12-29 15:49:53 -0700

    Bugfix newpost.py

--- _posts/newpost.py
+++ _posts/newpost.py
@@ -2,21 +2,20 @@
 
 # Derived from Bruce Eckel's NewPost.bat, https://github.com/BruceEckel/BruceEckel.github.io/blob/master/_posts/NewPost.bat
 
-
 import os, string, datetime, sys, subprocess
 
-def main(argv)
-  string = argv[0]
-  postname = string.lower().strip().replace(" ", "-")
+def main(argv):
+  name = argv[0]
+  postname = name.lower().strip().replace(" ", "-")
   postname = datetime.date.today().strftime("%Y-%m-%d-") + postname + ".md"
 
   slugline = """---
-  layout: post
-  published: false
-  title: %s
-  ---
+layout: post
+published: false
+title: %s
+---
 
-  """ % string.capwords(result)
+""" % string.capwords(name)
 
   with open(postname, 'w') as f:
       f.write(slugline)
